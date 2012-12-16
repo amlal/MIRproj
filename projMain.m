5bd1013913596065855fe28dc5255b2e5b2f9c51
amlal <amarmlal@gmail.com>
2012-12-15 19:35:04 -0500

    missing weighted diff

--- projMain.m
+++ projMain.m
@@ -1,30 +1,96 @@
-%run analysis on dataset
+%pass this an array containing an audio file at each point, and fs
+function [newDat] = projMain(library,target,fs,filename);
 
-%if dataset is large
-if numSongs > arbitrary
-    %pick a random distribution of songs
-    
+%run analysis on dataset
+for m =1:length(library)
+    alldata(m) = projAnalysis(library(m),fs);
 end
 
-%pick from data structure at indices songs, store in smaller data structure
+%analyze target
+targetDat = projAnalysis(target,fs);
+
+%buffer for output
 output = zeros(length(target),1);
 
-for 1:size(startend,2)
+%get sample lengths
+lengths = targetDat.endTime - targetDat.startTime;
+
+
+
+%for each sample in the target, find the closest from database
+for i = 1:length(targetDat.startTime)
+    
+    %starting distance high so first will be lower
+    bestmin = 5;
+    loc = [0,0];
+    
     %size of sample
-    sizeofsamp = startend(2,i)-startend(1,i);
-    %AL_EDist on measurements
-    %store in some matrix
-    %find min, use index to access dataset
-    samp = resample(data(starttime:endtime),sizeofsamp,length(x(starttime:endtime)); %TWEAK 4 VARS
+    sizeofsamp = lengths(i);
+    
+    %Distance measurements from entire library to single sample
+    for j = 1:length(library)
+        
+        %temporary for easier typing
+        temp = alldata(i);
+        
+        %rms distances
+       rmsD = abs(temp.rms - targetDat.rms(i));
+        %max chroma distance
+       maxChrD = abs(temp.maxChroma - targetDat.maxChroma(i));
+        %max amplitude difference
+       ampD = abs(temp.maxAmp - targetDat.maxAmp(i));
+        %chroma distance
+        chromD = zeros(length(temp.startTime),1);
+        for k = 1:length(temp.startTime)
+            %euclidean distance btw chroma vectors
+            chromD(k) = AL_EDist(temp.chroma(k,:),targetDat.chroma(i,:));
+        end
+        
+        %weighted differences
+        temp.weightedD = ;
+        
+        %if there's a distance closer than the closest found distance,
+        %store the index and value for future reference
+        [tempmin,temploc] = min(temp.weightedD);
+        if tempmin < bestmin
+            bestmin = tempmin;
+            %library file index, sample index
+            loc = [j,temploc];
+        end
+    end
+    
+    %get sample to use to avoid hairy coding again
+    touse = library(j).audioFile( (library(j).startTime(loc)) : (library(j).endTime(loc)) );
+    
+    %timestretch touse based on desired sample length           
+    touse = pvoc(touse, (sizeofsamp/length(touse)));
+    
     %concat samp to output
-    output(startend(1,i):startend(2,i)) = samp;
+    output( (targetDat.startTime(i)) : (targetDat.endTime(i)) ) = touse;
     
 end
 
 
 %run analysis on output
+newDat = projAnalysis(output,fs);
 %compare to target's analysis
-%store in matrix to ouput
 
-wavwrite(ouput,fs,16,'output');
+%rms distances
+rmsD = abs(targetDat.rms - newDat.rms);
+%max chroma distance
+maxChrD = abs(targetDat.maxChroma - newtDat.maxChroma);
+%max amplitude difference
+ampD = abs(targetDat.maxAmp - newtDat.maxAmp);
+%chroma distance
+chromD = zeros(length(targetDat.startTime),1);
+for n = 1:length(targetDat.startTime)
+    %euclidean distance btw chroma vectors
+    chromD(k) = AL_EDist(targetDat.chroma(n,:),newDat.chroma(n,:));
+end
+        
+%weighted differences
+newDat.weightedD =  ;
+
+%wavwrite output
+wavwrite(ouput,fs,16,filename);
     
